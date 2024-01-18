279bb1d9cd7ca573fc27280f55126b13183c14b5
Srinivas Edireswarapu - I17752 <srinivas.edireswarapu@microchip.com>
2024-01-18 15:01:21 +0530

    Added certificate tests

---
+++ test/atcacert/test_cert_def_4_device.h
@@ -0,0 +1,13 @@
+#ifndef TEST_CERT_DEF_4_DEVICE_H
+#define TEST_CERT_DEF_4_DEVICE_H
+
+#include "atcacert/atcacert_def.h"
+
+#if ATCACERT_INTEGRATION_EN
+
+extern const atcacert_def_t g_test_cert_def_4_device;
+extern const uint8_t g_test_ecc256_ca_cert[742];
+
+#endif /* ATCACERT_INTEGRATION_EN */
+
+#endif
---
+++ test/atcacert/test_cert_def_5_device.h
@@ -0,0 +1,8 @@
+#ifndef TEST_CERT_DEF_5_DEVICE_H
+#define TEST_CERT_DEF_5_DEVICE_H
+
+#include "atcacert/atcacert_def.h"
+
+extern const atcacert_def_t g_test_cert_def_5_device;
+extern const uint8_t g_test_ecc256_ca_cert_ecc608[408];
+#endif
